5f2f3467fb21c82e138ab4d9820d3e39951bf323
Daniel Petriu <danielpetriu75@gmail.com>
2021-01-26 11:51:15 -0300

    some changes

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+const jumpSpeed = 15;
 let isJumping = false;
 let position = 0;
+let speed = 5;
+
+let increaseSpeedInterval = setInterval(() => { speed += 5; }, 10000);
+
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function jump() {
   position = 0;
@@ -16,22 +24,23 @@ function jump() {
           clearInterval(downInterval);
           isJumping = false;
         } else {
-          position -= 20;
+          position -= jumpSpeed;
           dino.style.bottom = `${position}px`;
         }
       }, 20);
     } else {
       // up
-      position += 20;
+      position += jumpSpeed;
       dino.style.bottom = `${position}px`;
     }
   }, 20);
+
 }
 
 function createCactus() {
   const cactus = document.createElement('div');
-  let cactusPosition = 600;
-  let randomTime = Math.random() * 3000;
+  let cactusPosition = 800;
+  let randomTime = randomIntFromRange(2000, 4000);
 
   cactus.classList.add('cactus');
   cactus.style.left = `${cactusPosition}px`;
@@ -43,9 +52,10 @@ function createCactus() {
       background.removeChild(cactus);
     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
       clearInterval(leftInterval);
+      clearInterval(increaseSpeedInterval);
       document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
     } else {
-      cactusPosition -= 10;
+      cactusPosition -= speed;
       cactus.style.left = `${cactusPosition}px`;
     }
   }, 20);
@@ -53,7 +63,7 @@ function createCactus() {
   setTimeout(createCactus, randomTime);
 }
 
-function handleKeyUp(event) {
+function handleKeyDown(event) {
   const { keyCode } = event;
   if (keyCode == 32 && !isJumping) {
     jump();
@@ -61,4 +71,4 @@ function handleKeyUp(event) {
 }
 
 createCactus();
-document.addEventListener('keyup', handleKeyUp); 
+document.addEventListener('keydown', handleKeyDown);
